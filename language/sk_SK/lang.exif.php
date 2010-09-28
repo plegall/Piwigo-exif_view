98f0c25f18040600df2e2e8a68631b486fbcfaf2
ddtddt <ddtddt@piwigo.org>
2010-09-28 19:01:30 +0000

    [extension] exif_view add sk_SK Slovak (Slovensky) thanks to dodo Step 1
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@7031 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/lang.exif.php
@@ -0,0 +1,20 @@
+<?php
+//$lang_info['language_name'] = 'English';
+//$lang_info['country'] = ''Great Britain';
+//$lang_info['charset'] = 'iso-8859-1';
+//$lang_info['direction'] = 'ltr';
+//$lang_info['code'] = 'en';
+global $lang;
+
+$lang['exif_field_Make'] = 'Značka';
+$lang['exif_field_Model'] = 'Model';
+$lang['exif_field_DateTimeOriginal'] = 'Vytvorené';
+$lang['exif_field_ApertureFNumber'] = 'Clona';
+$lang['exif_field_ExposureTime'] = 'Čas';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO';
+$lang['exif_field_FocalLength'] = 'Ohnisko';
+$lang['exif_field_ExposureBiasValue'] = 'Korekcia';
+$lang['exif_field_ExposureProgram'] = 'Program';
+$lang['exif_field_Flash'] = 'Blesk';
+
+?>
