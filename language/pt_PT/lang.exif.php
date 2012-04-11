5f4f77d9ba1e548721925bb124acb1459f321858
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-11 21:08:21 +0000

    [Exif View] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@14103 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/lang.exif.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_field_Contrast'] = 'Contraste';
+$lang['exif_field_DateTimeOriginal'] = 'Data e hora do original';
+$lang['exif_field_DigitalZoomRatio'] = 'Zoom digital';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+?>
