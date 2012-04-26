3ec3958f7d6604e4ebf13ff3afb56d6ccd21b61b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-26 15:17:21 +0000

    [exif_view] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@14444 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/lang.exif.php
@@ -0,0 +1,99 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_field_Contrast'] = 'Contrast';
+$lang['exif_field_DateTimeOriginal'] = 'Dia i hora original';
+$lang['exif_field_DigitalZoomRatio'] = 'Zoom digital';
+$lang['exif_field_ExifVersion'] = 'Exif';
+$lang['exif_field_ExposureBiasValue'] = 'Compensació d\'exposició';
+$lang['exif_field_ExposureMode'] = 'Mode d\'exposició';
+$lang['exif_field_ExposureProgram'] = 'Programa d\'exposició';
+$lang['exif_field_ExposureTime'] = 'Temps d\'exposició';
+$lang['exif_field_FNumber'] = 'Número d\'obertura';
+$lang['exif_field_Flash'] = 'Flash';
+$lang['exif_field_FocalLengthIn35mmFilm'] = 'Distància focal per a pel·lícula de 35 mm';
+$lang['exif_field_FocalLength'] = 'Longitut focal';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO';
+$lang['exif_field_LightSource'] = 'Codi lumínic';
+$lang['exif_field_Make'] = 'Fabricant';
+$lang['exif_field_MeteringMode'] = 'Mode de medició de l\'exposició';
+$lang['exif_field_Model'] = 'Model';
+$lang['exif_field_Saturation'] = 'Saturació';
+$lang['exif_field_Sharpness'] = 'Nitidesa';
+$lang['exif_field_Software'] = 'Software EXIF';
+$lang['exif_field_SubjectDistance'] = 'Distància de l\'objecte';
+$lang['exif_field_UndefinedTag:0xA434'] = 'Lens';
+$lang['exif_field_WhiteBalance'] = 'Balanceig de blanc';
+$lang['exif_value_contrast_hard'] = 'dur';
+$lang['exif_value_contrast_normal'] = 'normal';
+$lang['exif_value_contrast_soft'] = 'suau';
+$lang['exif_value_exposure_mode_auto_bracket'] = 'enforquillament automàtic';
+$lang['exif_value_exposure_mode_auto'] = 'automàtic';
+$lang['exif_value_exposure_mode_manual'] = 'manual';
+$lang['exif_value_exposure_program_action'] = 'acció';
+$lang['exif_value_exposure_program_aperture'] = 'Prioritat d\'obertura';
+$lang['exif_value_exposure_program_creative'] = 'creatiu';
+$lang['exif_value_exposure_program_landscape'] = 'horitzontal';
+$lang['exif_value_exposure_program_manual'] = 'manual';
+$lang['exif_value_exposure_program_normal'] = 'normal';
+$lang['exif_value_exposure_program_not_defined'] = 'no difinit';
+$lang['exif_value_exposure_program_portrait'] = 'vertical';
+$lang['exif_value_exposure_program_shutter'] = 'Velocitat d\'obturació';
+$lang['exif_value_flash_mode_auto'] = 'automàtic';
+$lang['exif_value_flash_mode_compulsory'] = 'obligatori';
+$lang['exif_value_flash_mode_supress'] = 'suprimida';
+$lang['exif_value_flash_mode_unknown'] = 'desconegut';
+$lang['exif_value_flash_mode'] = 'mode';
+$lang['exif_value_flash_return_light_detected'] = 'llum de retorn detectada';
+$lang['exif_value_flash_return_light_not_detected'] = 'llum de retorn no detectada';
+$lang['exif_value_light_source_D50'] = 'D50';
+$lang['exif_value_light_source_D55'] = 'D55';
+$lang['exif_value_light_source_D65'] = 'D65';
+$lang['exif_value_light_source_D75'] = 'D75';
+$lang['exif_value_light_source_cloudy_weather'] = 'temps ennuvolat';
+$lang['exif_value_light_source_coolwhite_fluorescent_w'] = 'blanc fred (W 3900 - 4500K)';
+$lang['exif_value_light_source_daylight'] = 'Llum del dia';
+$lang['exif_value_light_source_fine_weather'] = 'bon temps';
+$lang['exif_value_light_source_flash'] = 'flash';
+$lang['exif_value_light_source_fluorescent'] = 'fluorescent';
+$lang['exif_value_light_source_iso_studio_tungsten'] = 'ISO estudi de tungstè';
+$lang['exif_value_light_source_other'] = 'altres';
+$lang['exif_value_light_source_shade'] = 'ombra';
+$lang['exif_value_light_source_standard_light_a'] = 'Llum estàndard A';
+$lang['exif_value_light_source_standard_light_b'] = 'Llum estàndard B';
+$lang['exif_value_light_source_standard_light_c'] = 'Llum estàndard C';
+$lang['exif_value_light_source_tungsten'] = 'tungstè';
+$lang['exif_value_light_source_unknown'] = 'desconegut';
+$lang['exif_value_metering_mode_CenterWeightedAVG'] = 'Centre mig';
+$lang['exif_value_metering_mode_average'] = 'Promig';
+$lang['exif_value_metering_mode_partial'] = 'parcial';
+$lang['exif_value_metering_mode_unknown'] = 'desconegut';
+$lang['exif_value_red_eye'] = 'reducció d\'ulls vermells';
+$lang['exif_value_saturation_hard'] = 'dur';
+$lang['exif_value_saturation_low'] = 'baix';
+$lang['exif_value_saturation_normal'] = 'normal';
+$lang['exif_value_sharpness_hard'] = 'dur';
+$lang['exif_value_sharpness_normal'] = 'normal';
+$lang['exif_value_sharpness_soft'] = 'suau';
+$lang['exif_value_white_balance_auto'] = 'automàtic';
+$lang['exif_value_white_balance_manual'] = 'manual';
+?>
