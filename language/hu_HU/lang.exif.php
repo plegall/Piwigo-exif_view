a39ecf2831947a711ea8f1314a5ebbb3abe41242
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-08 05:53:08 +0000

    [exif_view] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@14835 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/lang.exif.php
@@ -0,0 +1,83 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_field_Contrast'] = 'Kontraszt';
+$lang['exif_field_DateTimeOriginal'] = 'Eredeti dátum-idő';
+$lang['exif_field_DigitalZoomRatio'] = 'Digitális zoom';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+$lang['exif_field_ExposureBiasValue'] = 'Expozíció korrekció';
+$lang['exif_field_ExposureTime'] = 'Expozíciós idő';
+$lang['exif_field_FNumber'] = 'Rekesz érték';
+$lang['exif_field_Flash'] = 'Vaku';
+$lang['exif_field_FocalLengthIn35mmFilm'] = 'Fókusztávolság 35 mm-es rendszerben';
+$lang['exif_field_FocalLength'] = 'Fókusztávolság';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO (érzékenység)';
+$lang['exif_field_LightSource'] = 'Fényforrás';
+$lang['exif_field_Model'] = 'Modell';
+$lang['exif_field_Saturation'] = 'Telítettség';
+$lang['exif_field_Sharpness'] = 'Élesség';
+$lang['exif_field_Software'] = 'EXIF szoftver';
+$lang['exif_field_SubjectDistance'] = 'Tárgytávolság';
+$lang['exif_field_UndefinedTag:0xA434'] = 'Objektív';
+$lang['exif_field_WhiteBalance'] = 'Fehéregyensúly';
+$lang['exif_value_contrast_hard'] = 'kemény';
+$lang['exif_value_contrast_normal'] = 'normál';
+$lang['exif_value_contrast_soft'] = 'lágy';
+$lang['exif_value_exposure_mode_auto'] = 'auto';
+$lang['exif_value_exposure_mode_manual'] = 'kézi';
+$lang['exif_value_exposure_program_aperture'] = 'rekesz előválasztásos';
+$lang['exif_value_exposure_program_landscape'] = 'fekvő';
+$lang['exif_value_exposure_program_manual'] = 'kézi';
+$lang['exif_value_exposure_program_not_defined'] = 'definiálatlan';
+$lang['exif_value_exposure_program_portrait'] = 'álló';
+$lang['exif_value_exposure_program_shutter'] = 'idő előválasztásos';
+$lang['exif_value_flash_mode_auto'] = 'auto';
+$lang['exif_value_flash_mode_unknown'] = 'ismeretlen';
+$lang['exif_value_light_source_D50'] = 'D50';
+$lang['exif_value_light_source_D55'] = 'D55';
+$lang['exif_value_light_source_D65'] = 'D65';
+$lang['exif_value_light_source_D75'] = 'D75';
+$lang['exif_value_light_source_coolwhite_fluorescent_w'] = 'hidegfehér (W 3900 � 4500K)';
+$lang['exif_value_light_source_daylight_fluorescent_d'] = 'nappali fényű fénycső (D 5700 � 7100K)';
+$lang['exif_value_light_source_daylight'] = 'nappali fény';
+$lang['exif_value_light_source_daywhite_fluorescent_n'] = 'nappali fehér fénycső (N 4600 � 5400K)';
+$lang['exif_value_light_source_flash'] = 'vaku';
+$lang['exif_value_light_source_fluorescent'] = 'fénycső';
+$lang['exif_value_light_source_iso_studio_tungsten'] = 'ISO stúdió izzófény';
+$lang['exif_value_light_source_other'] = 'egyéb';
+$lang['exif_value_light_source_shade'] = 'árnyékolás';
+$lang['exif_value_light_source_tungsten'] = 'izzófény';
+$lang['exif_value_light_source_unknown'] = 'ismeretlen';
+$lang['exif_value_light_source_white_fluorescent'] = 'fehér fénycső (WW 3200 � 3700K)';
+$lang['exif_value_metering_mode_average'] = 'átlag';
+$lang['exif_value_metering_mode_partial'] = 'részleges';
+$lang['exif_value_metering_mode_pattern'] = 'mintázat';
+$lang['exif_value_metering_mode_unknown'] = 'ismeretlen';
+$lang['exif_value_red_eye'] = 'vörösszem csökkentés';
+$lang['exif_value_saturation_low'] = 'alacsony';
+$lang['exif_value_saturation_normal'] = 'normál';
+$lang['exif_value_sharpness_hard'] = 'kemény';
+$lang['exif_value_sharpness_normal'] = 'normál';
+$lang['exif_value_sharpness_soft'] = 'lágy';
+$lang['exif_value_white_balance_auto'] = 'auto';
+$lang['exif_value_white_balance_manual'] = 'kézi';
+?>
