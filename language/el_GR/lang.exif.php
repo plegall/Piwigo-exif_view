6942bbe86edee8c51708decad76790fa7cd52a38
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-20 21:30:06 +0000

    [exif_view] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@15163 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/lang.exif.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_field_Contrast'] = 'Αντίθεση';
+$lang['exif_field_DateTimeOriginal'] = 'Ημερομηνία Ώρα του Πρωτότυπου';
+$lang['exif_field_DigitalZoomRatio'] = 'Ψηφιακό Ζουμ';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+$lang['exif_field_ExposureBiasValue'] = 'Αντιστάθμιση Έκθεσης';
+$lang['exif_field_ExposureMode'] = 'Λειτουργία Έκθεσης';
+$lang['exif_field_ExposureProgram'] = 'Πρόγραμμα Έκθεσης';
+$lang['exif_field_ExposureTime'] = 'Χρόνος Έκθεσης';
+$lang['exif_field_FNumber'] = 'Αριθμός Διαφράγματος';
+$lang['exif_field_Flash'] = 'Φλας';
+$lang['exif_field_FocalLengthIn35mmFilm'] = 'Εστιακό Μήκος για  35mm φιλμ';
+$lang['exif_field_FocalLength'] = 'Εστιακό Μήκος';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO';
+$lang['exif_field_LightSource'] = 'Πηγή φωτός';
+$lang['exif_field_Make'] = 'Δημιουργός';
+$lang['exif_field_MeteringMode'] = 'Λειτουργία μέτρησης έκθεσης';
+$lang['exif_field_Model'] = 'Μοντέλο';
+$lang['exif_field_Saturation'] = 'Κορεσμός';
+$lang['exif_field_Sharpness'] = 'Σαφήνεια';
+$lang['exif_field_Software'] = 'EXIF λογισμικό';
+$lang['exif_field_SubjectDistance'] = 'Απόσταση Θέματος';
+$lang['exif_field_WhiteBalance'] = 'Εξισορρόπηση Λευκού';
+$lang['exif_value_contrast_hard'] = 'σκληρό';
+$lang['exif_value_contrast_normal'] = 'κανονικό';
+$lang['exif_value_contrast_soft'] = 'μαλακό';
+$lang['exif_value_exposure_mode_auto_bracket'] = 'αυτόματος βρόγχος';
+$lang['exif_value_exposure_mode_auto'] = 'αυτόματο';
+$lang['exif_value_exposure_mode_manual'] = 'χειροκίνητο';
+$lang['exif_value_exposure_program_action'] = 'ενέργεια';
+$lang['exif_value_exposure_program_aperture'] = 'προτεραιότητα διαφράγματος';
+$lang['exif_value_exposure_program_creative'] = 'δημιουργικό';
+$lang['exif_value_exposure_program_landscape'] = 'τοπίο';
+$lang['exif_value_exposure_program_manual'] = 'χειροκίνητο';
+?>
