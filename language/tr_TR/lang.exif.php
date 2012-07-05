d308066c56aae97a595a779958982b5d4de51267
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-05 15:57:51 +0000

    [exif_view] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@16375 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/lang.exif.php
@@ -0,0 +1,105 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_field_Contrast'] = 'Kontrast';
+$lang['exif_field_DateTimeOriginal'] = 'Orjinalin Saat ve Tarihi';
+$lang['exif_field_DigitalZoomRatio'] = 'SayısalYakınlaştırma';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+$lang['exif_field_ExposureBiasValue'] = 'Pozlama Telafisi';
+$lang['exif_field_ExposureMode'] = 'Pozlama Tarzı';
+$lang['exif_field_ExposureProgram'] = 'Pozlama Programı';
+$lang['exif_field_ExposureTime'] = 'Pozlama Süresi';
+$lang['exif_field_FNumber'] = 'Diyafram Numarası';
+$lang['exif_field_Flash'] = 'Flaş';
+$lang['exif_field_FocalLengthIn35mmFilm'] = '35mm Filmdeki Odak Uzaklığı';
+$lang['exif_field_FocalLength'] = 'Odak Uzaklığı';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO';
+$lang['exif_field_LightSource'] = 'Işık Kaynağı';
+$lang['exif_field_Make'] = 'Yapan';
+$lang['exif_field_MeteringMode'] = 'Pozlama Ölçüm Tarzı';
+$lang['exif_field_Model'] = 'Model';
+$lang['exif_field_Saturation'] = 'Canlılık';
+$lang['exif_field_Sharpness'] = 'Keskinlik';
+$lang['exif_field_Software'] = 'EXIF yazılımı';
+$lang['exif_field_SubjectDistance'] = 'Obje Uzaklığı';
+$lang['exif_field_UndefinedTag:0xA434'] = 'Lens';
+$lang['exif_field_WhiteBalance'] = 'Beyaz Dengesi';
+$lang['exif_value_contrast_hard'] = 'sert';
+$lang['exif_value_contrast_normal'] = 'normal';
+$lang['exif_value_contrast_soft'] = 'yumuşak';
+$lang['exif_value_exposure_mode_auto_bracket'] = 'otomatik pozlama';
+$lang['exif_value_exposure_mode_auto'] = 'otomatik';
+$lang['exif_value_exposure_mode_manual'] = 'manuel';
+$lang['exif_value_exposure_program_action'] = 'eylem';
+$lang['exif_value_exposure_program_aperture'] = 'diyafram önceliği';
+$lang['exif_value_exposure_program_creative'] = 'yaratıcı';
+$lang['exif_value_exposure_program_landscape'] = 'manzara';
+$lang['exif_value_exposure_program_manual'] = 'manuel';
+$lang['exif_value_exposure_program_normal'] = 'normal';
+$lang['exif_value_exposure_program_not_defined'] = 'tanımlanmamış';
+$lang['exif_value_exposure_program_portrait'] = 'portre';
+$lang['exif_value_exposure_program_shutter'] = 'obtüratör önceliği';
+$lang['exif_value_flash_mode_auto'] = 'otomatik';
+$lang['exif_value_flash_mode_compulsory'] = 'zorunlu';
+$lang['exif_value_flash_mode_supress'] = 'baskılanmış';
+$lang['exif_value_flash_mode_unknown'] = 'bilinmiyor';
+$lang['exif_value_flash_mode'] = 'tarz';
+$lang['exif_value_flash_return_light_detected'] = 'ışık dönüşü tesbit edildi';
+$lang['exif_value_flash_return_light_not_detected'] = 'ışık dönüşü tesbit edilmedi';
+$lang['exif_value_light_source_D50'] = 'D50';
+$lang['exif_value_light_source_D55'] = 'D55';
+$lang['exif_value_light_source_D65'] = 'D65';
+$lang['exif_value_light_source_D75'] = 'D75';
+$lang['exif_value_light_source_cloudy_weather'] = 'bulutlu hava';
+$lang['exif_value_light_source_coolwhite_fluorescent_w'] = 'soğuk beyaz (W 3900-4500K)';
+$lang['exif_value_light_source_daylight_fluorescent_d'] = 'gün ışığı floresan (D 5700-7100K)';
+$lang['exif_value_light_source_daylight'] = 'gün ışığı';
+$lang['exif_value_light_source_daywhite_fluorescent_n'] = 'gün beyazı floresan (N 4600-5400K)';
+$lang['exif_value_light_source_fine_weather'] = 'iyi hava';
+$lang['exif_value_light_source_flash'] = 'flaş';
+$lang['exif_value_light_source_fluorescent'] = 'floresan';
+$lang['exif_value_light_source_iso_studio_tungsten'] = 'ISO stüdyo tungsten';
+$lang['exif_value_light_source_other'] = 'diğer';
+$lang['exif_value_light_source_shade'] = 'gölge';
+$lang['exif_value_light_source_standard_light_a'] = 'standart ışık A';
+$lang['exif_value_light_source_standard_light_b'] = 'standart ışık B';
+$lang['exif_value_light_source_standard_light_c'] = 'standart ışık C';
+$lang['exif_value_light_source_tungsten'] = 'tungsten';
+$lang['exif_value_light_source_unknown'] = 'bilinmiyor';
+$lang['exif_value_light_source_white_fluorescent'] = 'beyaz floresan (WW 3200-3700K)';
+$lang['exif_value_metering_mode_CenterWeightedAVG'] = 'ortalamalı merkez';
+$lang['exif_value_metering_mode_average'] = 'ortalama';
+$lang['exif_value_metering_mode_multispot'] = 'multispot';
+$lang['exif_value_metering_mode_partial'] = 'parçalı';
+$lang['exif_value_metering_mode_pattern'] = 'kalıp';
+$lang['exif_value_metering_mode_spot'] = 'spot';
+$lang['exif_value_metering_mode_unknown'] = 'bilinmiyor';
+$lang['exif_value_red_eye'] = 'kırmızı göz önleme';
+$lang['exif_value_saturation_hard'] = 'sert';
+$lang['exif_value_saturation_low'] = 'düşük';
+$lang['exif_value_saturation_normal'] = 'normal';
+$lang['exif_value_sharpness_hard'] = 'sert';
+$lang['exif_value_sharpness_normal'] = 'normal';
+$lang['exif_value_sharpness_soft'] = 'yumuşak';
+$lang['exif_value_white_balance_auto'] = 'otomatik';
+$lang['exif_value_white_balance_manual'] = 'manuel';
+?>
