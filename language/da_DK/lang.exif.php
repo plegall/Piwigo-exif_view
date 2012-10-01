358cb8c573d7243e97b6a9f7d3b25010d14bef1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-01 21:16:27 +0000

    [exif_view] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@18416 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/lang.exif.php
@@ -0,0 +1,105 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_value_white_balance_auto'] = 'auto';
+$lang['exif_value_white_balance_manual'] = 'manuel';
+$lang['exif_value_light_source_standard_light_a'] = 'standardlys A';
+$lang['exif_value_light_source_standard_light_b'] = 'standardlys B';
+$lang['exif_value_light_source_standard_light_c'] = 'standardlys C';
+$lang['exif_value_light_source_tungsten'] = 'tungsten';
+$lang['exif_value_light_source_unknown'] = 'ukendt';
+$lang['exif_value_light_source_white_fluorescent'] = 'hvidt fluorescerende (WW 3200 – 3700K)';
+$lang['exif_value_metering_mode_CenterWeightedAVG'] = 'middel-centreret';
+$lang['exif_value_metering_mode_average'] = 'middel';
+$lang['exif_value_metering_mode_multispot'] = 'multispot';
+$lang['exif_value_metering_mode_partial'] = 'delvis';
+$lang['exif_value_metering_mode_pattern'] = 'mønster';
+$lang['exif_value_metering_mode_spot'] = 'spot';
+$lang['exif_value_metering_mode_unknown'] = 'ukendt';
+$lang['exif_value_red_eye'] = 'rødøjereduktion';
+$lang['exif_value_saturation_hard'] = 'hård';
+$lang['exif_value_saturation_low'] = 'lav';
+$lang['exif_value_saturation_normal'] = 'normal';
+$lang['exif_value_sharpness_hard'] = 'hård';
+$lang['exif_value_sharpness_normal'] = 'normal';
+$lang['exif_value_sharpness_soft'] = 'blød';
+$lang['exif_value_flash_mode_compulsory'] = 'tvungen';
+$lang['exif_value_flash_mode_supress'] = 'undertrykt';
+$lang['exif_value_flash_mode_unknown'] = 'ukendt';
+$lang['exif_value_flash_return_light_detected'] = 'returlys opdaget';
+$lang['exif_value_flash_return_light_not_detected'] = 'returlys ikke opdaget';
+$lang['exif_value_light_source_D50'] = 'D50';
+$lang['exif_value_light_source_D55'] = 'D55';
+$lang['exif_value_light_source_D65'] = 'D65';
+$lang['exif_value_light_source_D75'] = 'D75';
+$lang['exif_value_light_source_cloudy_weather'] = 'skyet vejr';
+$lang['exif_value_light_source_coolwhite_fluorescent_w'] = 'kølig hvid (W 3900 – 4500K)';
+$lang['exif_value_light_source_daylight'] = 'dagslys';
+$lang['exif_value_light_source_daylight_fluorescent_d'] = 'dag, let fluorescerende (D 5700 – 7100K)';
+$lang['exif_value_light_source_daywhite_fluorescent_n'] = 'dag, hvidt fluorescerende';
+$lang['exif_value_light_source_fine_weather'] = 'fint vejr';
+$lang['exif_value_light_source_flash'] = 'flash';
+$lang['exif_value_light_source_fluorescent'] = 'fluorescerende';
+$lang['exif_value_light_source_iso_studio_tungsten'] = 'ISO-studiotungsten';
+$lang['exif_value_light_source_other'] = 'andet';
+$lang['exif_value_light_source_shade'] = 'skygge';
+$lang['exif_field_SubjectDistance'] = 'Motivafstand';
+$lang['exif_field_UndefinedTag:0xA434'] = 'Objektiv';
+$lang['exif_field_WhiteBalance'] = 'Hvidbalance';
+$lang['exif_value_contrast_hard'] = 'hård';
+$lang['exif_value_contrast_normal'] = 'normal';
+$lang['exif_value_contrast_soft'] = 'blød';
+$lang['exif_value_exposure_mode_auto'] = 'auto';
+$lang['exif_value_exposure_mode_auto_bracket'] = 'autogruppe';
+$lang['exif_value_exposure_mode_manual'] = 'manuel';
+$lang['exif_value_exposure_program_action'] = 'action';
+$lang['exif_value_exposure_program_aperture'] = 'blændeprioritet';
+$lang['exif_value_exposure_program_creative'] = 'kreativ';
+$lang['exif_value_exposure_program_landscape'] = 'landskab';
+$lang['exif_value_exposure_program_manual'] = 'manuel';
+$lang['exif_value_exposure_program_normal'] = 'normal';
+$lang['exif_value_exposure_program_not_defined'] = 'ikke defineret';
+$lang['exif_value_exposure_program_portrait'] = 'portræt';
+$lang['exif_value_exposure_program_shutter'] = 'udløserprioritet';
+$lang['exif_value_flash_mode'] = 'tilstand';
+$lang['exif_value_flash_mode_auto'] = 'auto';
+$lang['exif_field_Contrast'] = 'Kontrast';
+$lang['exif_field_DateTimeOriginal'] = 'Originals optagelsestidspunkt';
+$lang['exif_field_DigitalZoomRatio'] = 'Digital zoom';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+$lang['exif_field_ExposureBiasValue'] = 'Eksponeringskompensation';
+$lang['exif_field_ExposureMode'] = 'Eksponeringstilstand';
+$lang['exif_field_ExposureProgram'] = 'Eksponeringsprogram';
+$lang['exif_field_ExposureTime'] = 'Eksponeringstid';
+$lang['exif_field_FNumber'] = 'Blændenummer';
+$lang['exif_field_Flash'] = 'Flash';
+$lang['exif_field_FocalLength'] = 'Brændvidde';
+$lang['exif_field_FocalLengthIn35mmFilm'] = 'Brændvidde i 35 mm-film';
+$lang['exif_field_ISOSpeedRatings'] = 'Film- eller sensorfølsomhed (ISO)';
+$lang['exif_field_LightSource'] = 'Lyskilde';
+$lang['exif_field_Make'] = 'Markør';
+$lang['exif_field_MeteringMode'] = 'Eksponeringsmålemetode';
+$lang['exif_field_Model'] = 'Model';
+$lang['exif_field_Saturation'] = 'Farvemætning';
+$lang['exif_field_Sharpness'] = 'Skarphed';
+$lang['exif_field_Software'] = 'EXIF-software';
+?>
