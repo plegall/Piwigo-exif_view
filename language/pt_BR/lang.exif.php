2e4cc8e5cb673f4ace3aae4e1bae1c38988a8985
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-26 06:01:45 +0000

    [exif_view] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/exif_view@21847 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/lang.exif.php
@@ -0,0 +1,105 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['exif_value_white_balance_auto'] = 'auto';
+$lang['exif_value_white_balance_manual'] = 'manual';
+$lang['exif_value_light_source_standard_light_a'] = 'luz padrão A';
+$lang['exif_value_light_source_standard_light_b'] = 'luz padrão B';
+$lang['exif_value_light_source_standard_light_c'] = 'luz padrão C';
+$lang['exif_value_light_source_tungsten'] = 'tungstênio';
+$lang['exif_value_light_source_unknown'] = 'desconhecido';
+$lang['exif_value_light_source_white_fluorescent'] = 'fluorescente branca (WW 3200 - 3700K)';
+$lang['exif_value_metering_mode_CenterWeightedAVG'] = 'centro média';
+$lang['exif_value_metering_mode_average'] = 'média';
+$lang['exif_value_metering_mode_multispot'] = 'múltiplos';
+$lang['exif_value_metering_mode_partial'] = 'parcial';
+$lang['exif_value_metering_mode_pattern'] = 'padrão';
+$lang['exif_value_metering_mode_spot'] = 'local';
+$lang['exif_value_metering_mode_unknown'] = 'desconhecido';
+$lang['exif_value_red_eye'] = 'redução de olhos vermelhos';
+$lang['exif_value_saturation_hard'] = 'forte';
+$lang['exif_value_saturation_low'] = 'baixo';
+$lang['exif_value_saturation_normal'] = 'normal';
+$lang['exif_value_sharpness_hard'] = 'forte';
+$lang['exif_value_sharpness_normal'] = 'normal';
+$lang['exif_value_sharpness_soft'] = 'leve';
+$lang['exif_value_flash_mode_compulsory'] = 'obrigatório';
+$lang['exif_value_flash_mode_supress'] = 'suprimida';
+$lang['exif_value_flash_mode_unknown'] = 'desconhecido';
+$lang['exif_value_flash_return_light_detected'] = 'retornar luz detectada';
+$lang['exif_value_flash_return_light_not_detected'] = 'retornar luz não detectada';
+$lang['exif_value_light_source_D50'] = 'D50';
+$lang['exif_value_light_source_D55'] = 'D55';
+$lang['exif_value_light_source_D65'] = 'D65';
+$lang['exif_value_light_source_D75'] = 'D75';
+$lang['exif_value_light_source_cloudy_weather'] = 'tempo nublado';
+$lang['exif_value_light_source_coolwhite_fluorescent_w'] = 'limpo fresco (W 3900 - 4500K)';
+$lang['exif_value_light_source_daylight'] = 'luz do dia';
+$lang['exif_value_light_source_daylight_fluorescent_d'] = 'dia luz fluorescente (D 5700 - 7100K)';
+$lang['exif_value_light_source_daywhite_fluorescent_n'] = 'fluorescente branca de dia (N 4600 - 5400K)';
+$lang['exif_value_light_source_fine_weather'] = 'tempo bom';
+$lang['exif_value_light_source_flash'] = 'flash';
+$lang['exif_value_light_source_fluorescent'] = 'fluorescente';
+$lang['exif_value_light_source_iso_studio_tungsten'] = 'ISO tungstênio estúdio';
+$lang['exif_value_light_source_other'] = 'outro';
+$lang['exif_value_light_source_shade'] = 'sombra';
+$lang['exif_field_SubjectDistance'] = 'Distância do Objetivo';
+$lang['exif_field_UndefinedTag:0xA434'] = 'Lente';
+$lang['exif_field_WhiteBalance'] = 'Equilíbrio Branco';
+$lang['exif_value_contrast_hard'] = 'pesado';
+$lang['exif_value_contrast_normal'] = 'normal';
+$lang['exif_value_contrast_soft'] = 'leve';
+$lang['exif_value_exposure_mode_auto'] = 'auto';
+$lang['exif_value_exposure_mode_auto_bracket'] = 'enquadramento automático';
+$lang['exif_value_exposure_mode_manual'] = 'manual';
+$lang['exif_value_exposure_program_action'] = 'ação';
+$lang['exif_value_exposure_program_aperture'] = 'prioridade de abertura';
+$lang['exif_value_exposure_program_creative'] = 'criador';
+$lang['exif_value_exposure_program_landscape'] = 'paisagem';
+$lang['exif_value_exposure_program_manual'] = 'manual';
+$lang['exif_value_exposure_program_normal'] = 'normal';
+$lang['exif_value_exposure_program_not_defined'] = 'não definida';
+$lang['exif_value_exposure_program_portrait'] = 'fotografia';
+$lang['exif_value_exposure_program_shutter'] = 'prioridade do obturador';
+$lang['exif_value_flash_mode'] = 'modo';
+$lang['exif_value_flash_mode_auto'] = 'auto';
+$lang['exif_field_Contrast'] = 'Contraste';
+$lang['exif_field_DateTimeOriginal'] = 'Data da Original';
+$lang['exif_field_DigitalZoomRatio'] = 'Zoom Digital';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+$lang['exif_field_ExposureBiasValue'] = 'Compensação de exposição';
+$lang['exif_field_ExposureMode'] = 'Modo de Exposição';
+$lang['exif_field_ExposureProgram'] = 'Programa de Exposição';
+$lang['exif_field_ExposureTime'] = 'Tempo de Exposição';
+$lang['exif_field_FNumber'] = 'Número de Abertura';
+$lang['exif_field_Flash'] = 'Flash';
+$lang['exif_field_FocalLength'] = 'Distância Focal';
+$lang['exif_field_FocalLengthIn35mmFilm'] = 'Distância focal em filme de 35mm';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO';
+$lang['exif_field_LightSource'] = 'Fonte de Luz';
+$lang['exif_field_Make'] = 'Fabricante';
+$lang['exif_field_MeteringMode'] = 'Modo de Medição de Exposição';
+$lang['exif_field_Model'] = 'Modelo';
+$lang['exif_field_Saturation'] = 'Saturação';
+$lang['exif_field_Sharpness'] = 'Nitidez';
+$lang['exif_field_Software'] = 'Software EXIF';
+?>
