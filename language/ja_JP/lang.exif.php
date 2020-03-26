0228c7ad7d8183a064da7d18ed284a635262cf0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-26 16:18:33 +0100

    Add ja_JP, thanks to Toramaru13

---
+++ language/ja_JP/lang.exif.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['exif_value_light_source_flash'] = 'フラッシュ';
+$lang['exif_value_light_source_daylight'] = '昼光';
+$lang['exif_value_light_source_fine_weather'] = '晴天';
+$lang['exif_value_light_source_D50'] = 'D50';
+$lang['exif_value_light_source_D55'] = 'D55';
+$lang['exif_value_light_source_D65'] = 'D65';
+$lang['exif_value_light_source_D75'] = 'D75';
+$lang['exif_value_light_source_cloudy_weather'] = '曇天';
+$lang['exif_value_exposure_program_normal'] = 'ノーマル';
+$lang['exif_value_exposure_program_not_defined'] = '定義されていない';
+$lang['exif_value_exposure_program_portrait'] = 'ポートレイト';
+$lang['exif_value_exposure_program_shutter'] = 'シャッターの優先度';
+$lang['exif_value_flash_mode'] = 'モード';
+$lang['exif_value_flash_mode_auto'] = 'オート';
+$lang['exif_value_flash_mode_unknown'] = '不明';
+$lang['exif_field_UndefinedTag:0xA434'] = 'レンズ';
+$lang['exif_field_WhiteBalance'] = 'ホワイトバランス';
+$lang['exif_value_contrast_hard'] = 'ハード';
+$lang['exif_value_contrast_normal'] = 'ノーマル';
+$lang['exif_value_contrast_soft'] = 'ソフト';
+$lang['exif_value_exposure_mode_auto'] = 'オート';
+$lang['exif_value_exposure_mode_manual'] = 'マニュアル';
+$lang['exif_value_exposure_program_action'] = 'アクション';
+$lang['exif_value_exposure_program_manual'] = 'マニュアル';
+$lang['exif_field_Contrast'] = 'コントラスト';
+$lang['exif_field_DateTimeOriginal'] = 'オリジナルの日時';
+$lang['exif_field_DigitalZoomRatio'] = 'デジタルズーム';
+$lang['exif_field_ExifVersion'] = 'EXIF';
+$lang['exif_field_ExposureMode'] = '露出モード';
+$lang['exif_field_Flash'] = 'フラッシュ';
+$lang['exif_field_FocalLength'] = '焦点距離';
+$lang['exif_field_FocalLengthIn35mmFilm'] = '35mmフィルムでの焦点距離';
+$lang['exif_field_ISOSpeedRatings'] = 'ISO';
+$lang['exif_field_Make'] = 'メーカー';
+$lang['exif_field_Model'] = 'モデル';
+$lang['exif_field_Saturation'] = '彩度';
+$lang['exif_field_Sharpness'] = 'シャープネス';
+$lang['exif_field_Software'] = 'EXIFソフトウェア';
